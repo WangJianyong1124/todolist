06feb0d9cb2d5ec9361a4303a727af5d4d13c329
WangJianyong1124 <19979014715@189.cn>
2021-10-07 14:08:37 +0800

    完成静态页面绘制

---
+++ src/components/Main/ListMain.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="todo-main">
+    <ListItem />
+  </ul>
+</template>
+
+<script>
+// 导入子组件Item
+import ListItem from './child/ListItem.vue'
+
+export default {
+  name: "ListMain",
+  components: {
+    ListItem
+  }
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
